2c16a5b76e870981d3655194f4f1025f29748a85
Sevket Ersan <sevketersan@hotmail.com>
2024-03-04 03:56:45 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  AccumulativeShadows,
+  RandomizedLight,
+  Center,
+  Environment,
+  OrbitControls,
+} from "@react-three/drei";
+import { useSpring, animated } from "@react-spring/three";
+import { MeshDistortMaterial } from "@react-three/drei";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Canvas shadows camera={{ position: [0, 0, 4.5], fov: 50 }}>
+      <group position={[0, -0.65, 0]}>
+        <Sphere />
+        <AccumulativeShadows
+          temporal
+          frames={200}
+          color="red"
+          colorBlend={0.5}
+          opacity={1}
+          scale={10}
+          alphaTest={0.85}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <RandomizedLight
+            amount={8}
+            radius={5}
+            ambient={0.5}
+            position={[5, 3, 2]}
+            bias={0.001}
+          />
+        </AccumulativeShadows>
+      </group>
+      <Env />
+      <OrbitControls
+        autoRotate
+        autoRotateSpeed={1}
+        enablePan={false}
+        enableZoom={false}
+        minPolarAngle={Math.PI / 2.1}
+        maxPolarAngle={Math.PI / 2.1}
+      />
+    </Canvas>
   );
 }
 
-export default App;
+function Sphere() {
+  const [clicked, setClicked] = useState(false);
+  const springs = useSpring({ color: clicked ? "#569AFF" : "#ff6d6d" });
+  const handleClick = () => setClicked((x) => !x);
+  const AnimatedMeshDistortMaterial = animated(MeshDistortMaterial);
+  return (
+    <Center top>
+      <mesh castShadow onClick={handleClick}>
+        <sphereGeometry args={[0.75, 64, 64]} />
+        <AnimatedMeshDistortMaterial
+          speed={3}
+          distort={0.6}
+          color={springs.color}
+        />
+      </mesh>
+    </Center>
+  );
+}
+
+function Env() {
+  return <Environment preset={"sunset"} background blur={1} />;
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <>
+    <h2 style={{ textAlign: "center" }}>3D Animation for Mood Diary</h2>
+    <p style={{ textAlign: "center" }}>Touch your mood!</p>
     <App />
-  </React.StrictMode>
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
